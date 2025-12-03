812ffd152094b18165b1bb224a9fb4ba9e02992e
Art <artem-volynkin06@mail.ru>
2025-12-03 23:25:52 +0300

    DB_change+.env add

--- airport_tracker/airport_tracker/urls.py
+++ airport_tracker/airport_tracker/urls.py
@@ -19,5 +19,8 @@ from frontend import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index,name='index')
+    path('',views.index,name='home'), #home,main
+    path('about/',views.about,name='home')
+    # path('about/',)
+    # main,about
 ]
--- airport_tracker/flights/models.py
+++ airport_tracker/flights/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class Flight(models.Model):
+    name='sample'
