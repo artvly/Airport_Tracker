fed3271327f21b947eabe7eb22a410a7e3f83f94
Art <artem-volynkin06@mail.ru>
2025-12-23 03:00:08 +0300

    start_fw_api

---
+++ airport_tracker/flights/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Flight
+
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Flight
+        fields = '__all__'
--- airport_tracker/flights/views.py
+++ airport_tracker/flights/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
+from .models import Flight
+from .serializers import ItemSerializer
+
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Flight.objects.all()
+    serializer_class = ItemSerializer
 # Create your views here.
